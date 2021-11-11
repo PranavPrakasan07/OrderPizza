b881aeee9bfcecc4fbd10e85a2d5b512a77f0c71
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-11-12 00:34:26 +0530

    Firebase init file

---
+++ js/firebaseInit.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB9E3uFoTlipw5i6Kx0P8dQLYyMhVtgUPs",
+    authDomain: "orderpizza-a21be.firebaseapp.com",
+    projectId: "orderpizza-a21be",
+    storageBucket: "orderpizza-a21be.appspot.com",
+    messagingSenderId: "950362202077",
+    appId: "1:950362202077:web:4118e6014f4f579fa2a034",
+    measurementId: "G-RQM88V4N7F"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
---
+++ js/userAuth.js
@@ -0,0 +1,181 @@
+function checkUser() {
+
+    var username = "user-";
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.User
+
+            const user = firebase.auth().currentUser;
+
+            var uid = user.uid;
+            username = user.email;
+
+            location.href = "../pages/aptitude.html";
+
+            // ...
+        } else {
+            // User is signed out
+            // ...
+            location.href = "../pages/login.html";
+
+        }
+    });
+}
+
+function fillUserData() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.User
+
+            document.getElementById('user_email').innerHTML = user.email;
+            document.getElementById('user_rno').innerHTML = localStorage.getItem("rno");
+
+        } else {
+
+        }
+    });
+}
+
+function validateDataLogin() {
+    var username = document.getElementById('l_username').value;
+    var password = document.getElementById('l_password').value;
+    var regno = document.getElementById('l_rno').value;
+
+    var isValid = validateData(username, password, regno, false);
+
+    if (isValid) {
+
+        firebase.auth().signInWithEmailAndPassword(username, password)
+            .then((userCredential) => {
+                // Signed in
+                // var user = userCredential.user;
+                const user = firebase.auth().currentUser;
+
+                // ...
+                alert("Hi, " + user.email);
+
+                location.href = '../pages/aptitude.html';
+
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                alert(errorMessage);
+            });
+
+    }
+}
+
+function validateDataSignin() {
+    var username = document.getElementById('s_username').value;
+    var password = document.getElementById('s_password').value;
+    var regno = document.getElementById('s_rno').value;
+
+    var isValid = validateData(username, password, regno, true);
+
+    if (isValid) {
+
+        firebase.auth().createUserWithEmailAndPassword(username, password)
+            .then((userCredential) => {
+                // Signed in 
+                // var user = userCredential.user;
+                const user = firebase.auth().currentUser;
+
+                // ...                
+
+                alert("Hi, " + user.email);
+
+                location.href = '../pages/dashboard.html';
+
+
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // ..
+
+                alert(errorMessage);
+
+            });
+
+    }
+}
+
+function validateData(username, password, regno, newAppl) {
+
+    var isCorrect = false;
+
+    var loginOrSignin = 'l'
+
+    if (newAppl) {
+        loginOrSignin = 's_'
+    } else {
+        loginOrSignin = 'l_'
+    }
+
+    if (username == "") {
+        document.getElementById(loginOrSignin + 'username_error').innerHTML = "*Please enter your VIT email";
+        isCorrect = false;
+    }
+
+    else if (!(username.includes("21@vitstudent.ac.in")
+        || username.includes("21a@vitstudent.ac.in")
+        || username.includes("21b@vitstudent.ac.in"))) {
+        document.getElementById(loginOrSignin + 'username_error').innerHTML = "*Please enter your VIT email";
+        isCorrect = false;
+    }
+
+    else {
+        document.getElementById(loginOrSignin + 'username_error').innerHTML = '&nbsp';
+        isCorrect = true;
+    }
+
+    if (password == "") {
+        document.getElementById(loginOrSignin + 'password_error').innerHTML = "*Please enter your password";
+        isCorrect = false;
+    }
+
+    else if (password.length < 6) {
+        document.getElementById(loginOrSignin + 'password_error').innerHTML = "*Password should be atleast 6 characters long";
+        isCorrect = false;
+    }
+
+    else {
+        document.getElementById(loginOrSignin + 'password_error').innerHTML = '&nbsp';
+        isCorrect = true;
+    }
+
+    if (regno == "") {
+        document.getElementById(loginOrSignin + 'rno_error').innerHTML = "*Please enter your registration number";
+        isCorrect = false;
+    }
+
+    else if (regno.length != 9) {
+        document.getElementById(loginOrSignin + 'rno_error').innerHTML = "*Fill in your correct registration number";
+        isCorrect = false;
+    }
+
+    else {
+        document.getElementById(loginOrSignin + 'rno_error').innerHTML = '&nbsp';
+        localStorage.setItem("rno", regno);
+        isCorrect = true;
+    }
+
+    return isCorrect;
+}
+
+function signOutUser() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        alert("Successfully signed out");
+        location.href = '../index.html';
+    }).catch((error) => {
+        // An error happened.
+        alert("Sign Out failed");
+    });
+}
+
